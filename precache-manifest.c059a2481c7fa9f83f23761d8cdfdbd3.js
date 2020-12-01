0816741f9824759c1865c108219ecb5c37626dd8
sean-binary <sean@deriv.com>
2020-12-01 18:01:17 +0800

    Updates

---
+++ precache-manifest.c059a2481c7fa9f83f23761d8cdfdbd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f9ddb0bc1b0620694dd43d50a380a34",
+    "url": "/object-recognition/index.html"
+  },
+  {
+    "revision": "3cc250dc02e6090cdf6b",
+    "url": "/object-recognition/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "75059484683cc2c7f9c8",
+    "url": "/object-recognition/static/js/2.131a8914.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/object-recognition/static/js/2.131a8914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cc250dc02e6090cdf6b",
+    "url": "/object-recognition/static/js/main.78d5db0d.chunk.js"
+  },
+  {
+    "revision": "f1d59179ca24c59de40a",
+    "url": "/object-recognition/static/js/runtime-main.48cf2aae.js"
+  }
+]);

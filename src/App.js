e31ada4b5077c0cee338bac0c74a4b11ff650709
sean-binary <sean@deriv.com>
2020-12-01 17:44:38 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React, { useRef, useState, useEffect } from "react";
 import * as tf from "@tensorflow/tfjs";
 // 1. TODO - Import required model here
 // e.g. import * as tfmodel from "@tensorflow-models/tfmodel";
+import * as cocossd from "@tensorflow-models/coco-ssd";
 import Webcam from "react-webcam";
 import "./App.css";
 // 2. TODO - Import drawing utility here
 // e.g. import { drawRect } from "./utilities";
+import { drawRect } from "./utilities";
 
 function App() {
   const webcamRef = useRef(null);
@@ -16,6 +18,7 @@ function App() {
   const runCoco = async () => {
     // 3. TODO - Load network 
     // e.g. const net = await cocossd.load();
+    const net = await cocossd.load();
     
     //  Loop and detect hands
     setInterval(() => {
@@ -45,14 +48,21 @@ function App() {
 
       // 4. TODO - Make Detections
       // e.g. const obj = await net.detect(video);
+      const obj = await net.detect(video);
 
       // Draw mesh
       const ctx = canvasRef.current.getContext("2d");
 
       // 5. TODO - Update drawing utility
-      // drawSomething(obj, ctx)  
+      // drawSomething(obj, ctx)
+      drawRect(obj, ctx); 
     }
   };
+
+  // Video Constraints for using back-facing camera
+  // const videoConstraints = {
+  //   facingMode: { exact: "user" }
+  // };
 
   useEffect(()=>{runCoco()},[]);
 
@@ -61,7 +71,8 @@ function App() {
       <header className="App-header">
         <Webcam
           ref={webcamRef}
-          muted={true} 
+          muted={true}
+          // videoConstraints={videoConstraints}
           style={{
             position: "absolute",
             marginLeft: "auto",

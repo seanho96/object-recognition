be1963da3a8a32e4dd01270a270440846ea3b4ce
Sean Ho <seanho96@yahoo.com>
2021-08-31 14:04:48 +0800

    Updates

---
+++ precache-manifest.62cbc06d39f4189c10e991ffd60134e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a44828ac768b386cf89c409b6f4fa881",
+    "url": "/object-recognition/index.html"
+  },
+  {
+    "revision": "ec78b388bee645fe4480",
+    "url": "/object-recognition/static/css/main.8af46472.chunk.css"
+  },
+  {
+    "revision": "14036abe48f290a872e4",
+    "url": "/object-recognition/static/js/2.48eb2d2e.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/object-recognition/static/js/2.48eb2d2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec78b388bee645fe4480",
+    "url": "/object-recognition/static/js/main.edb7945f.chunk.js"
+  },
+  {
+    "revision": "f1d59179ca24c59de40a",
+    "url": "/object-recognition/static/js/runtime-main.48cf2aae.js"
+  }
+]);
